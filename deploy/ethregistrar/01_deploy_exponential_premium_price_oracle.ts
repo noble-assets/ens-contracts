021e23a0688e8039c38cf768c39ec9412ddfb688
keyleu <kleuyckx@gmail.com>
2026-01-22 15:18:02 +0000

    add TODOs for the prices we are going to deploy with

--- deploy/ethregistrar/01_deploy_exponential_premium_price_oracle.ts
+++ deploy/ethregistrar/01_deploy_exponential_premium_price_oracle.ts
@@ -10,7 +10,7 @@ export default deployScript(
       const dummyOracle = await deploy('DummyOracle', {
         account: deployer,
         artifact: artifacts.DummyOracle,
-        args: [160000000000n],
+        args: [160000000000n], // TODO: Set NOBLE to USD price here, currently set to $1600
       })
       oracleAddress = dummyOracle.address
     }
@@ -20,8 +20,16 @@ export default deployScript(
       artifact: artifacts.ExponentialPremiumPriceOracle,
       args: [
         oracleAddress,
+        // TODO: Set how much names will cost here
+        // 1-2 chars: disabled
+        // Current prices are:
+        // 3 chars: $640$/year
+        // 4 chars: $160$/year
+        // 5+ chars: $5$/year
+        // Formula: price_per_second = (yearly_usd * 1e18) / 31536000
+        // Example for 3 chars: (640 * 1e18) / 31536000 = 20294266869609
         [0n, 0n, 20294266869609n, 5073566717402n, 158548959919n],
-        100000000000000000000000000n,
+        100000000000000000000000000n, // Premium decay during expiration time (starts with 100M USD) and expires in 21 days
         21n,
       ],
     })
